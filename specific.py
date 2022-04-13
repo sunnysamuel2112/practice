a97fdc3795992659a733aaeff93eeba61e00c169
sunnysamuelms <sunnysamuel2112@gmail.com>
2022-04-13 07:31:30 +0530

    added necessary comments

--- specific.py
+++ specific.py
@@ -1,24 +1,32 @@
 import tabula
 
+#taking filename
 filename = input("enter the filename with extension: ")
 
 i = 0
 
+#selecting the right dataframe
 df = tabula.read_pdf(filename, pages="all")[i]
 if df.columns[0] == 'University Seat Number':
     i = i+1
     df = tabula.read_pdf(filename, pages="all")[i]
 
+#create a dataframe list
 dataframe = []
 
+
+#appending Nan-less rows into the list
 for index, row in df.iterrows():
     if(row['Total'] >= 0):
         dataframe.append(row)
 
+#specifically fetch "Subject","Total","Result"
 marks = []
+
+#append each element of the Nan-less dataframe into marks
 for j in range(len(dataframe)):
     marks.append(dataframe[j][['Subject', 'Total', 'Result']])
 
-
+#printing out the values appended to marks
 for subs, tots,result in marks:
     print(subs, tots, result)
